75a208e12f8b07687b86bdbfd1149f71a0707e1a
Adi_uvu <102376281+Adi-uvu@users.noreply.github.com>
2024-08-19 18:09:41 +0200

    1234

--- beta-install.sh
+++ beta-install.sh
@@ -4,19 +4,6 @@ ROOT_UID=0
 DEST_DIR=""
 DESKTOP_ENV=""
 
-# Kolory ANSI do tęczowego tekstu
-RED='\033[0;31m'
-ORANGE='\033[0;33m'
-YELLOW='\033[1;33m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-RESET='\033[0m'  # Resetowanie koloru
-
-# Tabela kolorów w pionie
-COLORS=($RED $ORANGE $YELLOW $GREEN $CYAN $BLUE $PURPLE)
-
 # ASCII Art
 TEXT=(
 "              _ _                    "
@@ -29,15 +16,9 @@ TEXT=(
 "              |______|               "
 )
 
-# Wyświetlanie ASCII Art z kolorami pionowymi
-for (( i=0; i<${#TEXT[@]}; i++ )); do
-  LINE="${TEXT[$i]}"
-  for (( j=0; j<${#LINE}; j++ )); do
-    CHAR="${LINE:$j:1}"
-    COLOR=${COLORS[$((j % ${#COLORS[@]}))]}
-    echo -ne "${COLOR}${CHAR}${RESET}"
-  done
-  echo ""
+# Wyświetlanie ASCII Art
+for LINE in "${TEXT[@]}"; do
+  echo "$LINE"
 done
 
 # Funkcja do wykrywania środowiska graficznego
@@ -82,19 +63,19 @@ copy_ex_files() {
   case "$DESKTOP_ENV" in
     "GTK")
       DEST_DIR="/usr/share/backgrounds"
-      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      cp -pr build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       DEST_DIR="/usr/share/gnome-background-properties"
       cp -rT main/gtk $DEST_DIR
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane (protokół GTK)"
       ;;
     "QT")
       DEST_DIR="/usr/share/wallpapers"
-      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      cp -pr build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół QT)"
       ;;
     "fish")
       DEST_DIR="/usr/share/backgrounds/cutefishos"
-      cp -r build/wallpaper-0.3.24-ex/* "$DEST_DIR/"
+      cp -r build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół Fish[QT])"
       ;;
     "xfce")
@@ -104,21 +85,21 @@ copy_ex_files() {
       ;;
     "mate")
       DEST_DIR="/usr/share/backgrounds"
-      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      cp -pr build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       DEST_DIR="/usr/share/mate-background-properties"
       cp -rT main/gtk $DEST_DIR
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół GTK)"
       ;;
     "ubuntu-unity")
       DEST_DIR="/usr/share/backgrounds/ubuntu-unity"
-      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      cp -pr build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       DEST_DIR="/usr/share/gnome-background-properties"
       cp -rT main/unity $DEST_DIR
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół GTK)"
       ;;
     "ukui")
       DEST_DIR="/usr/share/backgrounds"
-      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      cp -pr build/wallpaper-0.3.24-ex/* $DEST_DIR/wallpaper-0.3.24
       DEST_DIR="/usr/share/ukui-background-properties"
       cp -rT main/gtk $DEST_DIR
       echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół UKUI[QT])"
@@ -155,7 +136,7 @@ case "$DESKTOP_ENV" in
     ;;
   "fish")
     DEST_DIR="/usr/share/backgrounds/cutefishos"
-    cp -r build/wallpaper-0.3.24/* "$DEST_DIR/"
+    cp -r build/wallpaper-0.3.24/* $DEST_DIR/wallpaper-0.3.24
     echo "Pliki zostały pomyślnie skopiowane... (protokół Fish[QT])."
     ;;
   "xfce")
