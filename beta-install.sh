d331f087c3d566463f6dc7195a43c3e21e5d1890
Adi_uvu <102376281+Adi-uvu@users.noreply.github.com>
2024-08-19 16:53:19 +0200

    Update beta-install.sh

--- beta-install.sh
+++ beta-install.sh
@@ -19,15 +19,14 @@ COLORS=($RED $ORANGE $YELLOW $GREEN $CYAN $BLUE $PURPLE)
 
 # ASCII Art
 TEXT=(
-"  /$$$$$$        /$$ /$$                                      "
-" /$$__  $$      | $$|__/                                      "
-"| $$  \\ $$  /$$$$$$$ /$$       /$$   /$$ /$$    /$$ /$$   /$$"
-"| $$$$$$$$ /$$__  $$| $$      | $$  | $$|  $$  /$$/| $$  | $$"
-"| $$__  $$| $$  | $$| $$      | $$  | $$ \\  $$/$$/ | $$  | $$"
-"| $$  | $$| $$  | $$| $$      | $$  | $$  \\  $$$/  | $$  | $$"
-"| $$  | $$|  $$$$$$$| $$      |  $$$$$$/   \\  $/   |  $$$$$$/"
-"|__/  |__/ \\_______/|__/      \\______/     \\_/     \\______/"
-"                       |______/                               "
+"              _ _                    "
+"     /\\      | (_)                   "
+"    /  \\   __| |_  _   ___   ___   _ "
+"   / /\\ \\ / _\` | || | | \\ \\ / / | | |"
+"  / ____ \\ (_| | || |_| |\\ V /| |_| |"
+" /_/    \\_\\__,_|_| \\__,_| \\_/  \\__,_|"
+"               ______                "
+"              |______|               "
 )
 
 # Wyświetlanie ASCII Art z kolorami pionowymi
@@ -41,15 +40,6 @@ for (( i=0; i<${#TEXT[@]}; i++ )); do
   echo ""
 done
 
-# Sprawdzanie, czy skrypt jest uruchamiany jako root
-if [ "$UID" -ne "$ROOT_UID" ]; then
-  echo "Ten skrypt wymaga uprawnien roota. Użyj sudo do uruchomienia skryptu."
-  exit 1
-fi
-
-# Wykrywanie środowiska graficznego
-detect_desktop_env
-
 # Funkcja do wykrywania środowiska graficznego
 detect_desktop_env() {
   case "$XDG_CURRENT_DESKTOP" in
@@ -59,89 +49,143 @@ detect_desktop_env() {
     "KDE" | "Lxqt")
       DESKTOP_ENV="QT"
       ;;
-    "Cutefish" | "Cutefish")
+    "Cutefish")
       DESKTOP_ENV="fish"
       ;;
-    "xfce" | "XFCE")
+    "XFCE")
       DESKTOP_ENV="xfce"
       ;;
-    "MATE" | "MATE")
+    "MATE")
       DESKTOP_ENV="mate"
       ;;
     "Unity:Unity7:ubuntu")
       DESKTOP_ENV="ubuntu-unity"
       ;;
-    "Ukui" | "UKUI")
+    "UKUI")
       DESKTOP_ENV="ukui"
       ;;
     *)
-      echo "Nie mozna wykryc srodowiska graficznego GTK lub QT..."
+      echo "Nie można wykryć środowiska graficznego GTK lub QT..."
       exit 1
       ;;
   esac
 }
 
-# Wykonywanie operacji na podstawie wykrytego środowiska
-if [ "$DESKTOP_ENV" = "GTK" ]; then
-  DEST_DIR="/usr/share/backgrounds"
-  cp -pr wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
-  DEST_DIR="/usr/share/gnome-background-properties"
-  cp -rT main/gtk $DEST_DIR
-  echo "Pliki zostały pomyślnie skopiowane (protokół GTK)."
-elif [ "$DESKTOP_ENV" = "QT" ]; then
-  DEST_DIR="/usr/share/wallpapers"
-  cp -pr wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
-  echo "Pliki zostały pomyślnie skopiowane... (protokół QT)."
-elif [ "$DESKTOP_ENV" = "fish" ]; then
-  DEST_DIR="/usr/share/backgrounds/cutefishos"
-  cp -r wallpaper-0.3.24/* "$DEST_DIR/"
-  echo "Pliki zostały pomyślnie skopiowane... (protokół Fish[QT])."
-elif [ "$DESKTOP_ENV" = "xfce" ]; then
-  DEST_DIR="/usr/share/xfce4/backdrops"
-  cp -r wallpaper-0.3.24/* "$DEST_DIR/"
-  echo "Pliki zostały pomyślnie skopiowane... (protokół XFCE[GTK])."
-elif [ "$DESKTOP_ENV" = "mate" ]; then
-  DEST_DIR="/usr/share/backgrounds"
-  cp -pr wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
-  DEST_DIR="/usr/share/mate-background-properties"
-  cp -rT main/gtk $DEST_DIR
-  echo "Pliki zostały pomyślnie skopiowane... (protokół GTK)."
-elif [ "$DESKTOP_ENV" = "ubuntu-unity" ]; then
-  DEST_DIR="/usr/share/backgrounds/ubuntu-unity"
-  cp -pr wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
-  DEST_DIR="/usr/share/gnome-background-properties"
-  cp -rT main/unity $DEST_DIR
-  echo "Pliki zostały pomyślnie skopiowane... (protokół GTK)."
-elif [ "$DESKTOP_ENV" = "ukui" ]; then
-  DEST_DIR="/usr/share/backgrounds"
-  cp -pr wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
-  DEST_DIR="/usr/share/ukui-background-properties"
-  cp -rT main/gtk $DEST_DIR
-  echo "Pliki zostały pomyślnie skopiowane... (protokół UKUI[QT])."
-else
-  echo "Nieobsługiwane środowisko graficzne."
+# Sprawdzanie, czy skrypt jest uruchamiany jako root
+if [ "$UID" -ne "$ROOT_UID" ]; then
+  echo "Ten skrypt wymaga uprawnien roota. Użyj sudo do uruchomienia skryptu."
   exit 1
 fi
 
-# Sprawdzanie opcji -rm lub -remove
-if [ "$1" = "-rm" ] || [ "$1" = "-remove" ]; then
-  if [ "$UID" -eq "$ROOT_UID" ]; then
-    # Usuwanie skopiowanych plików
-    if [ -d "/usr/share/backgrounds/wallpaper-0.3.24" ]; then
-      rm -rf /usr/share/backgrounds/wallpaper-0.3.24
-      echo "Usunięto pliki z /usr/share/backgrounds/"
-    fi
-    if [ -d "/usr/share/gnome-background-properties" ]; then
-      rm -rf /usr/share/gnome-background-properties/wallpaper-0.3.24
-      echo "Usunięto pliki z /usr/share/gnome-background-properties/"
-    fi
-    if [ -d "/usr/share/wallpapers" ]; then
-      rm -rf /usr/share/wallpapers/wallpaper-0.3.24
-      echo "Usunięto pliki z /usr/share/wallpapers/"
-    fi
-    # Dodaj inne ścieżki w razie potrzeby
-  else
-    echo "Ten skrypt wymaga uprawnień roota. Użyj sudo do uruchomienia skryptu."
-    exit 1
-  fi
+# Funkcja do kopiowania plików z katalogu build/wallpaper-0.3.24-ex
+copy_ex_files() {
+  case "$DESKTOP_ENV" in
+    "GTK")
+      DEST_DIR="/usr/share/backgrounds"
+      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      DEST_DIR="/usr/share/gnome-background-properties"
+      cp -rT main/gtk $DEST_DIR
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane (protokół GTK)"
+      ;;
+    "QT")
+      DEST_DIR="/usr/share/wallpapers"
+      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół QT)"
+      ;;
+    "fish")
+      DEST_DIR="/usr/share/backgrounds/cutefishos"
+      cp -r build/wallpaper-0.3.24-ex/* "$DEST_DIR/"
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół Fish[QT])"
+      ;;
+    "xfce")
+      DEST_DIR="/usr/share/xfce4/backdrops"
+      cp -r build/wallpaper-0.3.24-ex/* "$DEST_DIR/"
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół XFCE[GTK])"
+      ;;
+    "mate")
+      DEST_DIR="/usr/share/backgrounds"
+      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      DEST_DIR="/usr/share/mate-background-properties"
+      cp -rT main/gtk $DEST_DIR
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół GTK)"
+      ;;
+    "ubuntu-unity")
+      DEST_DIR="/usr/share/backgrounds/ubuntu-unity"
+      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      DEST_DIR="/usr/share/gnome-background-properties"
+      cp -rT main/unity $DEST_DIR
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół GTK)"
+      ;;
+    "ukui")
+      DEST_DIR="/usr/share/backgrounds"
+      cp -pr build/wallpaper-0.3.24-ex $DEST_DIR/wallpaper-0.3.24
+      DEST_DIR="/usr/share/ukui-background-properties"
+      cp -rT main/gtk $DEST_DIR
+      echo "Pliki rozrzeżone zostały pomyślnie skopiowane... (protokół UKUI[QT])"
+      ;;
+    *)
+      echo "Nieobsługiwane środowisko graficzne."
+      exit 1
+      ;;
+  esac
+}
+
+# Wykrywanie środowiska graficznego
+detect_desktop_env
+
+# Sprawdzanie opcji -ex
+if [ "$1" = "-ex" ]; then
+  copy_ex_files
+  exit 0
 fi
+
+# Wykonywanie operacji na podstawie wykrytego środowiska
+case "$DESKTOP_ENV" in
+  "GTK")
+    DEST_DIR="/usr/share/backgrounds"
+    cp -pr build/wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
+    DEST_DIR="/usr/share/gnome-background-properties"
+    cp -rT main/gtk $DEST_DIR
+    echo "Pliki zostały pomyślnie skopiowane (protokół GTK)."
+    ;;
+  "QT")
+    DEST_DIR="/usr/share/wallpapers"
+    cp -pr build/wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
+    echo "Pliki zostały pomyślnie skopiowane... (protokół QT)."
+    ;;
+  "fish")
+    DEST_DIR="/usr/share/backgrounds/cutefishos"
+    cp -r build/wallpaper-0.3.24/* "$DEST_DIR/"
+    echo "Pliki zostały pomyślnie skopiowane... (protokół Fish[QT])."
+    ;;
+  "xfce")
+    DEST_DIR="/usr/share/xfce4/backdrops"
+    cp -r build/wallpaper-0.3.24/* "$DEST_DIR/"
+    echo "Pliki zostały pomyślnie skopiowane... (protokół XFCE[GTK])."
+    ;;
+  "mate")
+    DEST_DIR="/usr/share/backgrounds"
+    cp -pr build/wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
+    DEST_DIR="/usr/share/mate-background-properties"
+    cp -rT main/gtk $DEST_DIR
+    echo "Pliki zostały pomyślnie skopiowane... (protokół GTK)."
+    ;;
+  "ubuntu-unity")
+    DEST_DIR="/usr/share/backgrounds/ubuntu-unity"
+    cp -pr build/wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
+    DEST_DIR="/usr/share/gnome-background-properties"
+    cp -rT main/unity $DEST_DIR
+    echo "Pliki zostały pomyślnie skopiowane... (protokół GTK)."
+    ;;
+  "ukui")
+    DEST_DIR="/usr/share/backgrounds"
+    cp -pr build/wallpaper-0.3.24 $DEST_DIR/wallpaper-0.3.24
+    DEST_DIR="/usr/share/ukui-background-properties"
+    cp -rT main/gtk $DEST_DIR
+    echo "Pliki zostały pomyślnie skopiowane... (protokół UKUI[QT])."
+    ;;
+  *)
+    echo "Nieobsługiwane środowisko graficzne."
+    exit 1
+    ;;
+esac
